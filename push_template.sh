467ca2a4a56360248fc7abb1dc3f154d3eb95b80
Kyle Ladd <kyleladd7@gmail.com>
2013-08-14 15:42:17 -0400

    Give push_template an argument

--- push_template.sh
+++ push_template.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-
-for pkg in $(ls -d */|sed "s/\///"); do
-    cp TEMPLATE.mk $pkg/$pkg.mk
+if [ $1 ]; then
+    cp TEMPLATE.mk $1/$1.mk
     pushd $pkg
-    sed -i.bak "s/PKGNAME/$pkg/g" $pkg.mk
+    sed -i.bak "s/PKGNAME/$1/g" $1.mk
     rm *.bak
     popd
-done
+else
+    for pkg in $(ls -d */|sed "s/\///"); do
+        cp TEMPLATE.mk $pkg/$pkg.mk
+        pushd $pkg
+        sed -i.bak "s/PKGNAME/$pkg/g" $pkg.mk
+        rm *.bak
+        popd
+    done
+fi
