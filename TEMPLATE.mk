5ea3d9ebd7ad8e44f80b95e1f622be1975b49e68
Kyle Ladd <kyleladd7@gmail.com>
2013-08-23 16:42:40 -0400

    TEMPLATE.mk: makepkg -s

--- TEMPLATE.mk
+++ TEMPLATE.mk
@@ -2,7 +2,7 @@
 .PHONY: PKGNAME clean-PKGNAME
 
 PKGNAME: PKGNAME/PKGBUILD
-	pushd PKGNAME; makepkg; popd
+	pushd PKGNAME; makepkg -s; popd
 
 clean-PKGNAME:
 	pushd PKGNAME; $(RM) -r pkg/ src/ *.tar.* *.patch; popd
