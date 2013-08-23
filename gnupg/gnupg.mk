d0d1aea0df19d86334827aacd7dc7a4caff97707
Kyle Ladd <kyleladd7@gmail.com>
2013-08-23 16:49:42 -0400

    gnupg: don't clean *.patch on make clean

--- gnupg/gnupg.mk
+++ gnupg/gnupg.mk
@@ -2,7 +2,7 @@
 .PHONY: gnupg clean-gnupg
 
 gnupg: gnupg/PKGBUILD
-	pushd gnupg; makepkg; popd
+	pushd gnupg; makepkg -s; popd
 
 clean-gnupg:
-	pushd gnupg; $(RM) -r pkg/ src/ *.tar.* *.patch; popd
+	pushd gnupg; $(RM) -r pkg/ src/ *.tar.*; popd
